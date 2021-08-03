337570ac02ccd3a508077fd9293b0fdda33575ee
Ashiqur Rahman <ashiq.starit@gmail.com>
2021-08-04 02:00:54 +0600

    first commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+import './styles.css'
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+
+const InputField: React.FC<Props> = ({todo, setTodo,handleAdd}) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    return (
+        <form className="input" onSubmit={(e) => {
+            handleAdd(e)
+            inputRef.current?.blur(); 
+        }}>
+            <input 
+            ref={inputRef}
+            type="input" 
+            placeholder='Enter a task' 
+            className="input__box"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+            />
+            <button 
+            className="input_submit" 
+            type="submit"
+
+            >Save</button>
+        </form>
+    )
+}
+
+export default InputField
